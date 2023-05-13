2ac0bff546608fdfe08c9d8c96060ffa8c114ff5
clew <lewis@clewup.co.uk>
2023-05-13 23:42:10 +0100

    feat(apps): add app linking with code authorization

--- src/components/AppCard/AppCard.tsx
+++ src/components/AppCard/AppCard.tsx
@@ -1,19 +1,45 @@
+'use client';
+
 import { AppType } from '@/types/appTypes';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import cx from 'classnames';
+import { SyncLoader } from 'react-spinners';
 
 interface AppCardProps {
     app: AppType;
 }
 
 const AppCard: FC<AppCardProps> = ({ app }) => {
+    const [isLoading, setLoading] = useState(false);
+    async function navigateToApp() {
+        setLoading(true);
+        const codeResponse = await fetch('/api/auth/code', { method: 'POST' });
+        const codeData = await codeResponse.json();
+        const code = codeData.authorization_code;
+        setLoading(false);
+
+        const formattedUrl = `${app.url}?code=${code}`;
+        window.open(formattedUrl, '_blank');
+    }
+
     return (
-        <Link href={app.url} target="_blank" className={cx({ 'pointer-events-none opacity-20': app.isDisabled })}>
-            <div className="w-64 h-64 border-2 border-black rounded-2xl m-2 flex flex-col justify-center items-center">
+        <div
+            className={cx(
+                'relative cursor-pointer w-64 h-64 border-2 border-black rounded-2xl m-2 flex flex-col justify-center items-center',
+                {
+                    'opacity-20 pointer-events-none': app.isDisabled,
+                }
+            )}
+            onClick={navigateToApp}>
+            {isLoading ? (
+                <span>
+                    <SyncLoader size={15} />
+                </span>
+            ) : (
                 <h1 className="text-5xl capitalize">{app.name}</h1>
-            </div>
-        </Link>
+            )}
+        </div>
     );
 };
 
