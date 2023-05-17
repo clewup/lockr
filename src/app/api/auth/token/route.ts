ccc273b070a97a0f294fdd3c53b063bbfb30f96b
clew <lewis@clewup.co.uk>
2023-05-17 20:52:08 +0100

    feat(token): add error and code to response

--- src/app/api/auth/token/route.ts
+++ src/app/api/auth/token/route.ts
@@ -9,12 +9,27 @@ export async function POST(request: Request) {
     const body = await request.json();
     const code = body.authorization_code;
 
+    enum TokenErrorCodes {
+        MISSING = '001',
+        NON_STRING = '002',
+        NO_USER_ID = '003',
+        PASSED_CODE_EXPIRED = '004',
+        NOT_EXISTS = '005',
+        VALID_CODE_EXPIRED = '006',
+    }
+
     // validate that a code has been passed in the body with the correct type
     if (!code) {
-        return response.json({}, { status: 400, statusText: 'Missing authorization_code' });
+        return response.json(
+            { error: 'Missing authorization_code', code: TokenErrorCodes.MISSING },
+            { status: 400, statusText: 'Missing authorization_code' }
+        );
     }
     if (typeof code !== 'string') {
-        return response.json({}, { status: 400, statusText: 'Invalid authorization_code' });
+        return response.json(
+            { error: 'Invalid authorization_code', code: TokenErrorCodes.NON_STRING },
+            { status: 400, statusText: 'Invalid authorization_code' }
+        );
     }
 
     const decodedToken = jwt.decode(code) as any;
@@ -24,11 +39,17 @@ export async function POST(request: Request) {
 
     // verify that the code, once decoded, contains the user's id
     if (!userId || typeof userId !== 'string') {
-        return response.json({}, { status: 400, statusText: 'Invalid authorization_code' });
+        return response.json(
+            { error: 'Invalid authorization_code', code: TokenErrorCodes.NO_USER_ID },
+            { status: 400, statusText: 'Invalid authorization_code' }
+        );
     }
     // verify that the code has not expired
     if (moment(expiry).isAfter(moment())) {
-        return response.json({}, { status: 400, statusText: 'Expired authorization_code' });
+        return response.json(
+            { error: 'Expired authorization_code', code: TokenErrorCodes.PASSED_CODE_EXPIRED },
+            { status: 400, statusText: 'Expired authorization_code' }
+        );
     }
 
     // query the database for a matching authorization code
@@ -41,11 +62,17 @@ export async function POST(request: Request) {
         },
     });
     if (!validCode) {
-        return response.json({}, { status: 400, statusText: 'Invalid authorization_code' });
+        return response.json(
+            { error: 'Invalid authorization_code', code: TokenErrorCodes.NOT_EXISTS },
+            { status: 400, statusText: 'Invalid authorization_code' }
+        );
     }
     // verify that the validated code has not expired
     if (moment(validCode.expires).isAfter(moment())) {
-        return response.json({}, { status: 400, statusText: 'Expired authorization_code' });
+        return response.json(
+            { error: 'Expired authorization_code', code: TokenErrorCodes.VALID_CODE_EXPIRED },
+            { status: 400, statusText: 'Expired authorization_code' }
+        );
     }
 
     // create the access token with the user information
