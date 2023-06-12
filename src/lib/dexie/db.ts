b487718d0b96a9a3e6c463a3c52708b342a63d73
clew <lewis@clewup.co.uk>
2023-06-12 17:57:11 -0400

    feat(lint): fix linting errors

--- src/lib/dexie/db.ts
+++ src/lib/dexie/db.ts
@@ -5,20 +5,27 @@ export type CookieConsent = {
     id?: number;
 };
 
+export type Disclaimer = {
+    id?: number;
+};
+
 export enum DexieTables {
     COOKIE_CONSENT = 'cookieConsent',
+    DISCLAIMER = 'disclaimer',
 }
 
 export class Dexie extends DexieBase {
     services: DexieServices;
 
     cookieConsent!: Table<CookieConsent>;
+    disclaimer!: Table<Disclaimer>;
 
     constructor() {
         super('lockr');
 
         this.version(1).stores({
             cookieConsent: '++id',
+            disclaimer: '++id',
         });
 
         this.services = new DexieServices(this);
